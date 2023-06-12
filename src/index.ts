65f28afbfc6fc4891c3d2e5a60557f4823c5eb30
farcek <farcek@gmail.com>
2023-06-12 13:45:17 +0800

    fix export ILogWriter

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { ILogItem, ILogAttr, OLogFactory } from "./common";
 import { LogLevel, LogColor } from "./level";
-
 import { Logger, LogBuilder } from "./logger";
 import { factoryLogManager, sampleLogWriter, parseLogLevel } from "./manager";
+import { ILogWriter } from "./writer";
 
 export const LogManager = factoryLogManager();
 
 export {
-    ILogItem, ILogAttr, OLogFactory,
+    ILogItem, ILogAttr, OLogFactory, ILogWriter,
     LogLevel, LogColor,
     Logger, LogBuilder,
     factoryLogManager, sampleLogWriter, parseLogLevel
--- test/ww.ts
+++ test/ww.ts
@@ -5,9 +5,16 @@ LogManager.addWriter({
     level: LogLevel.trace,
     writer: sampleLogWriter()
 })
+LogManager.addWriter({
+    logname : "teste",
+    level: LogLevel.trace,
+    writer: sampleLogWriter()
+})
 
 let log = getLogger("teste.adf");
+let log1 = getLogger("other");
 
 
 
 log.i("test out")
+log1.i("test out")
